027b37c622150f78aaa700ba3a1077a0e4609f4f
cocotyty <cocotyty@sina.com>
2016-05-30 00:46:26 +0800

    update for documents

---
+++ doc.go
@@ -0,0 +1,20 @@
+//#> What is summer ?
+//
+//Summer is a easy tool if you just want a simple IOC/DI framework.
+//Summer work based on tag "sm".
+//
+//If you want to use summer,you must put stone to the summer basket first,
+//
+//and you must call "Start",then summer can work for you.
+//
+//
+//#> What is Stone ?
+//
+//Stone is go stone,like bean is javabean.
+//
+//
+//#> What is basket ?
+//
+//A container to store stone,and resolve stones dependents.
+//
+package summer
